149adee1a84a725ea5f1974760bb812e09137d41
Paulo Gustavo Sousa Brito <oluap077@gmail.com>
2023-08-24 11:03:48 -0300

    Leitura de dados com o scanner

--- src/Condicional.java
+++ src/Condicional.java
@@ -2,7 +2,7 @@ public class Condicional {
     public static void main(String[] args) {
         int anoDeLancamento = 2022;
         boolean incluidoNoPlano = true;
-        double notaDoFilme = 8.1;
+        // double notaDoFilme = 8.1;
         String tipoPlano = "plus";
 
         if (anoDeLancamento >= 2022){
